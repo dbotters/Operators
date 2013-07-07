e211e80a561481bce814bbfa3c89700c8b81279f
David <David@DAVIDs-MacBook-Pro.local>
2013-07-06 22:55:38 -0400

    Initial Commit

---
+++ Operators/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Operators
+//
+//  Created by David on 7/6/13.
+//  Copyright (c) 2013 David. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
